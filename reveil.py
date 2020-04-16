696f44d3d1c55be0f37faa94c1fc31699c4691f4
yannick <yaland@gmail.com>
2020-04-16 13:32:25 +0200

    Update reveil.py

--- reveil.py
+++ reveil.py
@@ -1,5 +1,4 @@
-
-import time, string, sys, threading, os
+import time, string, sys, threading, os, re
 
 from kalliope.core.NeuronModule import NeuronModule, InvalidParameterException
 
@@ -17,45 +16,34 @@ class Reveil (NeuronModule):
 
         """Donne l'heure courante au format hh:mm """
         self.temps = time.strftime("%H:%M")
-#       self.say("Il est " + self.temps)
-        
-        """ 
-                - Ordre: "Réveille-moi à dix heures 30"
-                    Kalliope renvoie 10h30
-                    je doit reformater la chaine pour avoir 10:30
-                    J'utilise "str.replace('h',':')"
-                - Ordre: "Réveille-moi à 11 heures"
-                    Kalliope renvoie 11h
-                    je doit reformater la chaine pour avoir 11:00
-                    Je verifie la longueur de la chaine "len()" et j'utilise "str.split('h')"
-                - Ordre: "Réveille moi à neuf heures 50"
-                    Kalliope renvoie 9h50
-                    Je verifie la longueur de la chaine "len()" et je rajoute un "0" 
-        """
-
-        test_heure_reveil = len(self.heure_reveil)
-        print("Longeur de la chaine: " + str(test_heure_reveil))
-        print(self.heure_reveil)
-        if test_heure_reveil == 3:
-                self.reveil = self.heure_reveil.split('h')
-                self.reveil= self.reveil[0] + ":00"
-        elif test_heure_reveil == 4:
-                self.reveil = self.heure_reveil.replace('h',':')
-                self.reveil= "0" + self.reveil
+ 
+        if re.match(r'(..)h(..)',self.heure_reveil) or re.match(r'(..)h',self.heure_reveil) == None: 
+                self.say("Ce n'est pas une heure valide!")
         else:
-                self.reveil = self.heure_reveil.replace('h',':')
-                print(self.reveil)
+        	test_heure_reveil = len(self.heure_reveil)
+	        print("Longeur de la chaine: " + str(test_heure_reveil))
+        	print(self.heure_reveil)
+        	if test_heure_reveil == 3:
+        	        self.reveil = self.heure_reveil.split('h')
+        	        self.reveil= self.reveil[0] + ":00"
+        	elif test_heure_reveil == 4:
+        	        self.reveil = self.heure_reveil.replace('h',':')
+        	        self.reveil= "0" + self.reveil
+        	else:
+        	        self.reveil = self.heure_reveil.replace('h',':')
+        	print(self.reveil)
+       		self.say("Le réveil est programmé à " + self.reveil )
+        	test = True
+        	while(test == True):
+        	        heure_courante = time.strftime("%H:%M")
+        	        print(heure_courante)
+        	        print(self.reveil)
+        	        time.sleep(1)
+        	        if(self.reveil == heure_courante):
+        	                print("OK")
+        	                os.system("bash /home/pi/barbara/scripts_ya/franceInter.sh")  
+        	                test = False
 
-        self.say("Réveil programmé à " + self.reveil )
-        test = True
-        while(test == True):
-                heure_courante = time.strftime("%H:%M")
-                print(heure_courante)
-                print(self.reveil)
-                time.sleep(1)
-                if(self.reveil == heure_courante):
-                        print("OK")
-#                        os.system("/usr/bin/mplayer -slave -quiet http://direct.franceinter.fr/live/franceinter-midfi.mp3 &")
-                        os.system("bash /home/pi/barbara/scripts_ya/franceInter.sh")  
-                        test = False
+
+
 
